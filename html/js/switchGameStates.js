4e233853e4f07ff2efe308e1443be00c73c4b3be
FabiK97 <34299329+FabiK97@users.noreply.github.com>
2018-04-12 13:13:32 +0200

    Add files via upload
    Basic project design without functions

---
+++ html/js/switchGameStates.js
@@ -0,0 +1,36 @@
+var inGame;
+var pgselect;
+var pselect;
+var menu;
+
+
+var menuButton;
+var psButton;
+var pgsButton;
+var ingameButton;
+
+window.onload = function() {
+    inGame = document.getElementById("g1");
+    pgselect = document.getElementById("pgs");
+    pselect = document.getElementById("ps");
+    menu = document.getElementById("m");
+
+    ingameButton = document.getElementById("ingameButton");
+    menuButton = document.getElementById("menuButton");
+    psButton = document.getElementById("psButton");
+    pgsButton = document.getElementById("pgsButton");
+
+    ingameButton.onclick = function() {show(inGame, pgselect, pselect, menu)};
+    pgsButton.onclick = function() {show(pgselect, inGame, pselect, menu)};
+    psButton.onclick = function() {show(pselect, pgselect, inGame, menu)};
+    menuButton.onclick = function() {show(menu, inGame, pgselect, pselect)};
+};
+
+function show(el1, el2, el3, el4) {
+    console.log("click");
+        el1.style.display = 'block';
+        el2.style.display = 'none';
+        el3.style.display = 'none';
+        el4.style.display = 'none';
+}
+
